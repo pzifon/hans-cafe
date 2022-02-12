03ed951757a5fc7c99a2dc30beacb0d2c70b0b6f
Pee Zhi Feng <kelvinpee00@gmail.com>
2022-02-13 00:50:49 +0800

    front end edit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,6 +1,5 @@
 <style>
     ul {
-        height: 50px;
         list-style-type: none;
         margin: 0;
         padding: 0;
@@ -30,30 +29,29 @@
 </style>
 
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100" style="position:fixed;width:100%">
-            <ul>
-                <li><a href="/">Hans Cafe</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/booking">Booking</a></li>
-                @if (Route::has('login'))
-                @auth
-                    <li style="float:right">
-                        <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                            <a class="active" onclick="event.preventDefault();this.closest('form').submit();":href="route('logout')"
-                            class="text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
-                        </form>
-                    </li>
-                    <li style="float:right"><a class="active" href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Account</a></li>
-                    <li style="float:right"><a class="active" href="{{ url('/cart') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Cart</a></li>
-                @else
-                    <li style="float:right"><a class="active" href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
-                @if (Route::has('register'))
-                    <li style="float:right"><a class="active" href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
-                @endif
-                @endauth
-                @endif
-            </ul>
-            <!-- <x-slot name="content">
+    <ul>
+        <li><a href="/">Hans Cafe</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/booking">Booking</a></li>
+        @if (Route::has('login'))
+        @auth
+        <li style="float:right">
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a class="active" onclick="event.preventDefault();this.closest('form').submit();" :href="route('logout')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log out</a>
+            </form>
+        </li>
+        <li style="float:right"><a class="active" href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Account</a></li>
+        <li style="float:right"><a class="active" href="{{ url('/cart') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Cart</a></li>
+        @else
+        <li style="float:right"><a class="active" href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a></li>
+        @if (Route::has('register'))
+        <li style="float:right"><a class="active" href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a></li>
+        @endif
+        @endauth
+        @endif
+    </ul>
+    <!-- <x-slot name="content">
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 

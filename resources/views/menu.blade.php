cc30224731834999538138fa2a9bfb846a88dbe0
Nicholas <nicholastan301@gmail.com>
2022-04-23 17:02:16 +0800

    Enter

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -11,7 +11,7 @@
 
 <body>
     @include('layouts.navbar')
-    <div stlye="height:50px"></div>
+    <div stlye="height:500px"></div>
     <div class="container">
         <h1 style="font-size:20px">Main Course</h1>
         <div class="row row-cols-1 row-cols-md-3 g-4">

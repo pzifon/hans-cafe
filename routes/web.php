07e91e5ab3a0cee2d7054052fcd5a6e689db38c2
Peiyii01 <ngpeiyii@gmail.com>
2022-04-23 11:00:23 +0800

    Employee clock in & out

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,17 @@ Route::get('/', function () {
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/dashboard','App\Http\Controllers\DashboardController@index')->name('dashboard');
 });
-// Route::get('/dashboard','App\Http\Controllers\DashboardController@index');
 
 Route::group(['middleware' => ['auth', 'role:customer']], function() {
     Route::get('/editacc', 'App\Http\Controllers\DashboardController@viewCustinfo')->name('editAcc');
     Route::post('edit', 'App\Http\Controllers\DashboardController@editCustInfo')->name('editAccFunction');
+    Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
+    Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
+    Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
+    Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
+    Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
+    Route::post('insert', [CartController::class, 'insertToDatabase'])->name('cart.insert');
 });
-// Route::get('/editacc', 'App\Http\Controllers\DashboardController@viewCustinfo');
-// Route::post('edit', 'App\Http\Controllers\DashboardController@editCustInfo');
 
 Route::get('/purchases', 'App\Http\Controllers\PurchaseController@index');
 Route::post('viewDetails','App\Http\Controllers\PurchaseController@viewDetails');
@@ -41,13 +44,14 @@ Route::post('create','App\Http\Controllers\BookingController@insert');
 Route::post('cancel','App\Http\Controllers\BookingController@remove');
 
 Route::get('/menu', [MenuController::class, 'menuList'])->name('menu.list');
-Route::get('cart', [CartController::class, 'cartList'])->name('cart.list');
-Route::post('cart', [CartController::class, 'addToCart'])->name('cart.store');
-Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.update');
-Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
-Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
-Route::post('insert', [CartController::class, 'insertToDatabase'])->name('cart.insert');
 
-Route::get('/order', 'App\Http\Controllers\MenuController@orderMenu');
+Route::group(['middleware' => ['auth', 'role:employee']], function() {
+    Route::get('/order', 'App\Http\Controllers\MenuController@orderMenu')->name('order');
+    Route::get('/clockIn', 'App\Http\Controllers\ClockInController@clockIn');
+    Route::get('/clockOut', 'App\Http\Controllers\ClockInController@clockOut');
+});
 
+Route::group(['middleware' => ['auth', 'role:admin']], function() {
+    Route::get('/order', 'App\Http\Controllers\MenuController@orderMenu')->name('order');
+});
 require __DIR__.'/auth.php';

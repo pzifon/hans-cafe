251e2b56eac20d107291aaf26813d22c192f4069
Peiyii01 <ngpeiyii@gmail.com>
2022-04-25 10:52:56 +0800

    Fix nav bar

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,6 @@ Route::group(['middleware' => ['auth', 'role:customer']], function() {
     Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
     Route::post('insert', [CartController::class, 'insertToDatabase'])->name('cart.insert');
 
-    Route::get('/booking', function () {
-        return view('booking');
-    });
     Route::post('create','App\Http\Controllers\BookingController@insert');
     Route::post('cancel','App\Http\Controllers\BookingController@remove');
 });
@@ -45,6 +42,10 @@ Route::get('/purchases', 'App\Http\Controllers\PurchaseController@index');
 Route::post('viewDetails','App\Http\Controllers\PurchaseController@viewDetails');
 
 Route::get('/menu', [MenuController::class, 'menuList'])->name('menu.list');
+
+Route::get('/booking', function () {
+    return view('booking');
+});
 
 Route::group(['middleware' => ['auth', 'role:employee']], function() {
     Route::get('/clockIn', 'App\Http\Controllers\ClockInController@clockIn');

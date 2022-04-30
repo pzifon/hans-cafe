00e879c7c2bae6dd9962ffe48fe533ca90c9ba29
pingwei918 <pingwei918@gmail.com>
2022-04-30 22:40:08 +0800

    Merge branch 'main' of https://github.com/pzifon/hans-cafe into pingwei

--- resources/views/cust/dashboard.blade.php
+++ resources/views/cust/dashboard.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html style="position:relative;min-height:89vh">
 
 <head>
     <title>Account Dashboard</title>
@@ -11,18 +11,18 @@
     </style>
 </head>
 
-<body>
+<body style="margin-bottom:60px">
     @include('layouts.navbar')
     @include('flash-message')
     <div class="container" style="margin-top:20px; margin-bottom: 20px">
-        <div class="row"">
+        <div class="row">
             <button class=" btn btn-success col" style="margin:10px" href="/purchases">
-            <div class="row">
-                <div class="col"><i class="bi bi-wallet2"></i></div>
-                <div class="col">{{ $total_purchases}}</br>Total Purchase</div>
-            </div>
+                <div class="row">
+                    <div class="col"><i class="bi bi-wallet2"></i></div>
+                    <div class="col">{{ $total_purchases}}</br>Total Purchase</div>
+                </div>
             </button>
-            <button class="btn btn-success col" style="margin:10px" href="#">
+            <button class="btn btn-success col" style="margin:10px" href="/reward">
                 <div class="row">
                     <div class="col"><i class="bi bi-award"></i></div>
                     <div class="col">2</br>Rewards</div>
@@ -144,9 +144,8 @@
             </div>
         </div>
     </div>
-    <div class="mt-auto">
-    @include('layouts.footer')
-    </div>
+    <br>
+    
 </body>
 
 </html>

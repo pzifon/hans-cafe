23c65733ff15fd22f3c33d2ffcc8fae8e679450c
Pee <kelvinpee00@gmail.com>
2022-06-22 00:01:07 +0800

    orderlist change 2.01

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -19,14 +19,14 @@ class OrderController extends Controller
             $purchase_id = DB::table('purchases')
                 ->insertGetId(['customer_id'  => Auth::id(), 'date' => date('Y-m-d'), 'total' => $request->price]);
 
-            foreach ($item as $key => $value){
+            foreach ($item as $key => $value) {
                 $add_data = array('purchase_id' => $purchase_id, "menu_code" => $value["menu_code"], "price" => $value["price"], "quantity" => 1, 'subtotal' => $value["price"]);
                 DB::table('orders')->insert($add_data);
             }
 
             return response()->json([
                 'status' => 'success',
-                'data' => $data, 
+                'data' => $data,
                 'item' => $item,
                 'add_data' => $add_data,
             ]);
@@ -37,53 +37,54 @@ class OrderController extends Controller
         }
     }
 
-    // public function orderList()
-    // {
-    //     $orderList = DB::table('purchases')
-    //         -> select('*')
-    //         -> where('payment_status', false)
-    //         -> where('date', date("Y-m-d"))
-    //         -> get();
-    //     $orders = DB::table('orders')
-    //         ->join('purchases', 'purchases.id', '=', 'orders.purchase_id')
-    //         ->select('orders.*')
-    //         ->get(); 
-    //     foreach ($orders as $o){
-    //         $items = DB::table('menus')
-    //         ->join('orders', 'orders.menu_code', '=', 'menus.menu_code')
-    //         ->select('menus.name', 'menus.menu_code')
-    //         ->get(); 
-    //     }
-    //     Debugbar::info($items);
-    //     return view('orderlist', compact('orderList', 'orders', 'items'));
-    // }
+    public function orderList()
+    {
+        $orderList = DB::table('purchases')
+            ->select('*')
+            ->where('payment_status', false)
+            ->where('date', date("Y-m-d"))
+            ->get();
+        $orders = DB::table('orders')
+            ->join('purchases', 'purchases.id', '=', 'orders.purchase_id')
+            ->select('orders.*')
+            ->get();
+        foreach ($orders as $o) {
+            $items = DB::table('menus')
+                ->join('orders', 'orders.menu_code', '=', 'menus.menu_code')
+                ->select('menus.name', 'menus.menu_code')
+                ->get();
+        }
+        Debugbar::info($items);
+        return view('orderlist', compact('orderList', 'orders', 'items'));
+    }
 
-    // public function orderDetails($id)
-    // {
-    //     $orderList = DB::table('orders')
-    //         -> select('*')
-    //         -> where('payment_status', false)
-    //         -> where('date', date("Y-m-d"))
-    //         -> get();
-    //     return view('orderlist', compact('orderList'));
-    // }
-
-    public function getOrder() {
+    public function getOrder()
+    {
         $order = DB::table('purchases')
-            -> select('id')
-            -> where('purchases.payment_status', false)
-            -> where('date', date("Y-m-d"))
-            -> get();
+            ->select('id')
+            ->where('purchases.payment_status', false)
+            ->where('date', date("Y-m-d"))
+            ->get();
 
-        $details = DB::table('orders')
-            -> select('*')
-            -> where('orders.purchase_id', $order->id)
-            -> get();
+        foreach ($order as $key) {
+            $id = array('id' => $key->id);
+            $details = DB::table('orders')
+                ->select('*')
+                ->where('orders.purchase_id', $id)
+                ->get();
+            
+        }
+
+        // $details = DB::table('orders')
+        //     ->select('*')
+        //     ->where('orders.purchase_id', $orderid)
+        //     ->get();
 
         return response()->json([
             'data' => "Success",
             'order' => $order,
             'details' => $details,
+            'id' => $id,    
         ]);
     }
 }
